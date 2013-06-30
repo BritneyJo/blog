c21889653a1ea511a5ddffc86ebac10aa6d5ae34
bj <britneyjo.ludkowski@gmail.com>
2013-06-30 12:36:31 +0100

    working view all page

--- main.rb
+++ main.rb
@@ -4,18 +4,35 @@ require 'sinatra/contrib/all'
 require 'pg'
 require 'haml'
 
-db = PG.connect(dbname: 'todo_database', host: 'localhost')
+db = PG.connect(dbname: 'blogposts', host: 'localhost')
+
+configure do
+  enable :session
+end
 
 get '/' do
+  sql = "select * from posts"
+  @results = db.exec(sql)
 haml :index
 end
 
-get '/posts' do
-end
-
 get '/post/:id' do
+  sql = "select * from posts where id = #{params[:id]} limit 1"
+  @result = deb.exec(sql).first
+  haml :view_post
 end
 
-post '/post/:id/edit' do
-  puts 'homepage'
-end
+# post '/post/:id/edit' do
+#   puts 'homepage'
+# end
+
+# get '/admin_connect' do
+#   if params["login"]== "admin1" and params["password"]=="wdi"
+#     session[:admin] = true
+#     redirect "/admin" #page not created yet...
+# end
+
+# get '/admin' do
+#   unless session[:admin] == true
+#     redirect "/"
+# end
