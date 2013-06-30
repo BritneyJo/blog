fcc3a96d9de4876c4a93ba1aa9be753dfb86daae
bj <britneyjo.ludkowski@gmail.com>
2013-06-30 16:49:10 +0100

    main.rb edited

--- main.rb
+++ main.rb
@@ -16,22 +16,12 @@ get '/' do
 haml :index
 end
 
-get '/post/:id' do
-  sql = "SELECT * FROM posts WHERE id=#{params[:id]} limit 1"
-  @results = db.exec(sql).first
-  haml :view_post
-end
-
-post '/post/:id/edit' do
-  puts 'post has been updated'
-end
-
 get '/admin_connect' do
   if params["login"] == "admin1" and params["password"]=="wdi"
     session[:admin] = true
-    redirect "/"
-    haml :admin_connect
+    redirect "/admin"
   end
+  haml :admin_connect
 end
 
 get '/admin' do
@@ -39,3 +29,45 @@ get '/admin' do
     redirect "/"
   end
 end
+
+
+get '/create_post' do
+  haml :create_post
+end
+
+post '/create_post' do
+    if (params[:title].length > 0)
+      sql = "insert into posts (title, content, creation_date, author) values ('#{params[:title]}', '#{params[:content]}', '#{params[:creation_date]}', '#{params[:author]}')"
+      @results = db.exec(sql)
+    redirect to '/'
+  else
+    haml :create_post
+  end
+end
+
+get '/delete/:id' do
+  sql = "delete from posts where id = #{params[:id]}"
+  @results = db.exec(sql)
+  haml :delete_post
+end
+
+get '/post/:id' do
+  sql = "SELECT * FROM posts WHERE id=#{params[:id]} limit 1"
+  @results = db.exec(sql).first
+  haml :view_post
+end
+
+
+get '/update/:id' do
+    sql = "select * from posts where id = #{params[:id]} limit 1"
+      @results = db.exec(sql).first
+  haml :edit
+end
+
+post '/update/:id' do
+  sql = "update posts set title='#{params[:title]}', content='#{params[:content]}', creation_date='#{params[:creation_date]}', author='#{params[:author]}' where id='#{params[:id]}'"
+  @results = db.exec(sql)
+  redirect "/post/#{params[:id]}"
+  haml :edit
+end
+
